564a4efa347b378c1478a15e21ced528181feda3
Salvador Alvarez <sao.95@hotmail.com>
2022-07-19 10:47:47 -0700

    pages

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './routes/App';
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App tab="home" />);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./routes/App.jsx";
+
+
+// const root = ReactDOM.createRoot(document.getElementById("app"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
