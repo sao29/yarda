d7104828ddba1d9b75ae3e1fe0dd7b39476ba757
Salvador Alvarez <sao.95@hotmail.com>
2022-07-19 12:13:09 -0700

    prueba seis

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter , Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -19,7 +19,7 @@ const App = () => {
 	const intialState = useInitialState();
 	return (
 		<AppContext.Provider value={intialState} >
-		<BrowserRouter basename='/yarda'>
+		<HashRouter  >
 			<Layout>
 				<Switch>
 					<Route exact path="/" component={Home} />
@@ -34,7 +34,7 @@ const App = () => {
 					<Route path="*" component={NotFound} />
 				</Switch>
 			</Layout>
-		</BrowserRouter>
+		</HashRouter>
 		</AppContext.Provider>
 	);
 }
